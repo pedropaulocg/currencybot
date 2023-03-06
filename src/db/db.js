fbd4d806ba7db3f9bc371225681db8806dc64be9
pedropaulocg <pedropaulocostaguima@hotmail.com>
2023-03-06 20:40:54 -0300

    conexao com banco de dados em nuvem

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2")
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
   database: 'currencybot',
-  username: 'qgcemyzlri5htp0bsob0',
+  user: 'qgcemyzlri5htp0bsob0',
   host: 'aws-sa-east-1.connect.psdb.cloud',
-  password: 'pscale_pw_WM7ICH4S2Nws2sO58kfT1GNrg1te1UyxD4z2TlfnRwa'
+  password: 'pscale_pw_WM7ICH4S2Nws2sO58kfT1GNrg1te1UyxD4z2TlfnRwa',
+  ssl: {}
 })
 
 module.exports = connection
