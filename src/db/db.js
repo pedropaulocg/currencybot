d9cd5e7a463b8bef82814dcfd88f3a723ed311e8
pedropaulocg <pedropaulocostaguima@hotmail.com>
2023-03-06 20:33:05 -0300

    teste db em nuvem

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2")
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  database: 'currencybot'
+const connection = mysql.createPool({
+  database: 'currencybot',
+  username: 'qgcemyzlri5htp0bsob0',
+  host: 'aws-sa-east-1.connect.psdb.cloud',
+  password: 'pscale_pw_WM7ICH4S2Nws2sO58kfT1GNrg1te1UyxD4z2TlfnRwa'
 })
 
 module.exports = connection
